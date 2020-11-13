855fb1be11a075b4b40d7cb50eb302d01b20a685
Chetan Badhe <csbadhe08@gmail.com>
2020-11-13 10:42:09 +0530

    ft: eth faucet

--- api/release.js
+++ api/release.js
@@ -1,54 +1,77 @@
-const axios = require("axios");
 const cors = require('micro-cors')()
 const ethers = require('ethers')
+const axios = require('axios')
 
-const ERC20ABI = [  
-    {
-    constant: false,
-    inputs: [
-      {
-        name: '_to',
-        type: 'address',
-      },
-      {
-        name: '_value',
-        type: 'uint256',
-      },
-    ],
-    name: 'transfer',
-    outputs: [
-      {
-        name: '',
-        type: 'bool',
-      },
-    ],
-    payable: false,
-    stateMutability: 'nonpayable',
-    type: 'function',
-  }
-]
+const config = {
+	privateKey: '0xe5d49951d72b64ecf999d198333539a6b7af0717d0ada784d0badf3ff3c27f94',
+	infuraKey: 'a4dcdfe968254cd4a2a30381e3558541',
+	ethFaucetAddress: '0x4E6521F2653fEDb02Bc33e1cBB34BD41A953D0Bf'
+}
 
-const infuraKey = "a4dcdfe968254cd4a2a30381e3558541";
-const provider =  new ethers.providers.JsonRpcProvider(`https://kovan.infura.io/v3/${infuraKey}`)
-const privateKey = '0x332b7e2caf3932a9d4fc97ccbb08a195d855b51cfa3af20f4a2696cc20319df1'
+const infuraKey = config.infuraKey;
+const provider = new ethers.providers.JsonRpcProvider(`https://kovan.infura.io/v3/${infuraKey}`)
+const privateKey = config.privateKey
 const signer = new ethers.Wallet(privateKey, provider);
 
 const handler = async (req, res) => {
-    const address = req.body.address
-    console.log(req.body)
-    const testDai = new ethers.Contract('0x5124d2A8e3A02f906d86803D703FD6CcCf492EF8', ERC20ABI, signer)
-    const testEth = new ethers.Contract('0x6468Cb5b76200428A514125BfA5a08Cf2E4b7f9D', ERC20ABI, signer)
-    try {
-        const testDaiTransfer = await testDai.transfer(address, "100000000000000000000")
-        const testEthTransfer = await testEth.transfer(address, "100000000000000000000")
-        res.status(200).send({
-            dai: testDaiTransfer,
-            eth: testEthTransfer
-        })
-    } catch (error) {
-        console.log(error)
-        res.status(400).send("Something is wrong")
-    }
+	const address = req.body.address
+
+	try {
+		const etherScanApiUrl = 'https://api-kovan.etherscan.io/api'
+		const params = {
+			module: 'account',
+			action: 'txlist',
+			address,
+			startblock: '0',
+			endblock: '99999999',
+			sort: 'desc',
+			apikey: 'HUWMR5VJHDQ7EEZYEUWQAAHBNMURE1R1CH',
+		}
+		const result = await axios.get(etherScanApiUrl, {
+			params
+		})
+
+		let i;
+
+		console.log(result.data.result.length)
+
+
+		if (result.data.result.length === 0) {
+			const transaction = await signer.sendTransaction({
+				to: address,
+				value: ethers.utils.parseEther("0.05")
+			});
+			res.status(200).send(transaction)
+		} else {
+			for (i = 0; i < result.data.result.length; i++) {
+				console.log(result.data.result[i].from)
+				if (result.data.result[i].from.toLowerCase() === config.ethFaucetAddress.toLowerCase()) {
+					const currentTimeStamp = Math.round(+new Date() / 1000)
+					if (currentTimeStamp >= parseInt(result.data.result[i].timeStamp) + 86400) {
+						const transaction = await signer.sendTransaction({
+							to: address,
+							value: ethers.utils.parseEther("0.05")
+						});
+						res.status(200).send(transaction)
+					} else {
+						res.status(400).send('Not allowed')
+					}
+					break;
+				} else {
+					const transaction = await signer.sendTransaction({
+						to: address,
+						value: ethers.utils.parseEther("0.05")
+					});
+					res.status(200).send(transaction)
+					break;
+				}
+			}
+		}
+
+	} catch (error) {
+		console.log(error)
+		res.status(400).send("Something is wrong")
+	}
 }
 
 module.exports = cors(handler)
